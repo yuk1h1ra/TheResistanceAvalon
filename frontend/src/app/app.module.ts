f3cd773df1e56f61cf49ef826c198a1d1fbe30ee
yuk1h1ra <koheishoji913@gmail.com>
2018-10-30 01:17:19 +0900

    finished without CORS

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { 
   MatToolbarModule,
@@ -29,6 +30,7 @@ import { CreateComponent } from './create/create.component';
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
